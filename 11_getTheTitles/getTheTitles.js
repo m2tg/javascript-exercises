db3d045709b472134afade19cf23b89614021284
m2tg <monishrvps2008@gmail.com>
2023-07-15 00:39:50 +0530

    Completed excercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(input) {
+    bookTitles = [];
 
+    bookTitles = input.map(element => element.title);
+
+    return bookTitles;
+    
 };
 
 // Do not edit below this line
