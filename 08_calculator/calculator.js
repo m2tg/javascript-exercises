2d4b37edd9b9421b7f5053fc158d7d2470fa0b8e
m2tg <monishrvps2008@gmail.com>
2023-07-14 23:16:02 +0530

    Completed excercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
+const sum = function(arr) {
 	
+  let arrsum = 0;
+
+  arr.forEach(element => {
+    arrsum += element;
+  });
+
+  return arrsum;
 };
 
-const multiply = function() {
+const multiply = function(a,...args) {
+  
+  args.forEach(element => {
+    a *= element;
+  });
 
+  return a;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
+  let fact = 1;
+
+  for (let i = 1; i<=num; i++) {
+    fact *= i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
