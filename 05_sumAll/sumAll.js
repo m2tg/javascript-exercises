e7e4a93781e0232d159ea9eb23eae5993be0cc2c
m2tg <monishrvps2008@gmail.com>
2023-07-14 21:25:19 +0530

    Completed excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function( start, end) {
+    
+    if ((start < 0) || (end < 0) ) {
+        return 'ERROR';
+    }
 
+    if ( !(Number.isInteger(start)) || !((Number.isInteger(end)))) {
+        return 'ERROR'
+    }
+
+    if(start > end) {
+        start = start + end;
+        end = start - end;
+        start = start - end;
+    }
+    console.log(start);
+    
+    let sum = 0;
+
+    while ( start <= end) {
+        sum += start;
+        start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
