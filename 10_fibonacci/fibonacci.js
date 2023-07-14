fde3d5efd4dd965efed78a77941150ae8ce609ca
m2tg <monishrvps2008@gmail.com>
2023-07-15 00:26:17 +0530

    Completed excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    num = parseInt(num);
+
+    if (num<0) {
+        return 'OOPS';
+    }
+    let first = 1
+      , next = 1;
+
+    for ( let i = 3; i <= num; i++) {
+        next = first + next;
+        first = next - first;
+    }
+    return next;
 };
 
 // Do not edit below this line
