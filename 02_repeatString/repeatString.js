8f58b91a93ab425c38ef58c9af9b2341264c00b6
m2tg <monishrvps2008@gmail.com>
2023-07-14 20:28:38 +0530

    Completed excercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, numberOfTimes) {
+    
+    let repeatedString = '';
 
+    if (numberOfTimes < 0) {
+        return 'ERROR';
+    }
+    
+    while(numberOfTimes > 0){
+        repeatedString += str;
+        numberOfTimes--;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
