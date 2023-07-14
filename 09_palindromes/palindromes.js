983e327382812d4c7c6457851c4af470390f9184
m2tg <monishrvps2008@gmail.com>
2023-07-15 00:13:04 +0530

    Completed excercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    
+    let cleanStr = input.toLowerCase();
+    cleanStr = cleanStr.replaceAll(/[^a-z0-9]/g,"");
+    //console.log(cleanStr);
 
+    let reversedString = '';
+
+    for (let i = cleanStr.length; i >= 0; i--) {
+        
+        reversedString += cleanStr.charAt(i);
+    }
+    console.log(reversedString);
+    
+    return cleanStr == reversedString;
 };
 
 // Do not edit below this line
