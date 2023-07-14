2861eedde24afedb61228933cb83b2169c57e179
m2tg <monishrvps2008@gmail.com>
2023-07-14 21:08:31 +0530

    Completed excercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+   
+    const newArray = []; 
 
+    arr.forEach((element) => {
+        if (!(args.includes(element))){
+            newArray.push(element);
+        }
+    });
+    console.log(newArray);
+    return newArray;
 };
 
 // Do not edit below this line
