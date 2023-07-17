0fd7ba19ccc198837a1647013d00b246999cdeed
m2tg <monishrvps2008@gmail.com>
2023-07-18 00:41:13 +0530

    Completed excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    
+    input.sort((a,b) => {
+        if((!b.yearOfDeath) && (a.yearOfBirth > b.yearOfBirth)){
+            return 1;
+        }
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        return bAge > aAge ? 1 : -1;
+    })
+    
+    return input[0];
 };
 
 // Do not edit below this line
